c2c7f3ca71fb2df11efcfe991b334078201af848
TailaiYing <taiying9627@gmail.com>
2024-07-18 20:51:11 -0600

    committed!

--- app/(overview)/progress/page.tsx
+++ app/(overview)/progress/page.tsx
@@ -3,7 +3,7 @@
 export default function Page() {
     return (
         <div>
-            <p>hello world from progress</p>
+            <p>hello world from progress testing to see if vercvel works</p>
         </div>
     )
 }
